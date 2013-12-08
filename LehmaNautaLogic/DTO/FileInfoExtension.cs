14d13f3aff54e5bc4d06dc39634881261a5faa99
LosManos <ola.fjelddahl@gmail.com>
2013-12-08 12:12:01 +0100

    Docs. Wrote comments.

--- LehmaNautaLogic/DTO/FileInfoExtension.cs
+++ LehmaNautaLogic/DTO/FileInfoExtension.cs
@@ -4,6 +4,11 @@ namespace LehmaNautaLogic.DTO
 {
 	public static class FileInfoExtension
 	{
+		/// <summary>This method ensures we have an ID.
+		/// If there already is an ID we let it be.
+		/// </summary>
+		/// <param name="me"></param>
+		/// <returns></returns>
 		public static Guid EnsureID(this FileInfo me)
 		{
 			if (me.Id == Guid.Empty)
@@ -13,6 +18,13 @@ namespace LehmaNautaLogic.DTO
 			return me.Id;
 		}
 
+		/// <summary>This method sets all properties
+		/// for a fully populated object.
+		/// </summary>
+		/// <param name="me"></param>
+		/// <param name="filename"></param>
+		/// <param name="owner"></param>
+		/// <returns></returns>
 		public static FileInfo Set(
 			this FileInfo me, 
 			string filename,
