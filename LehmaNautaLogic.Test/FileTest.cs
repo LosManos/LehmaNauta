77da1ce0f6e992ff79ba94a2e674edab59063b57
LosManos <ola.fjelddahl@gmail.com>
2013-12-09 22:01:49 +0100

    "physical" files module with tests implemented.

---
+++ LehmaNautaLogic.Test/FileTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LNL = LehmaNautaLogic;
+
+namespace LehmaNautaLogic.Test
+{
+	[TestClass]
+	public class FileTest
+	{
+		private const string RepositoryPath = @"..\..\TI.RepoFolder";
+
+		[TestMethod]
+		public void FullRobin()
+		{
+			const string SourcePathfile = @"..\..\Incoming\Fullrobin.txt";
+			CreateSourceFile(SourcePathfile);
+
+			var testee = new LNL.FileService(RepositoryPath);
+			var id = Guid.NewGuid();
+
+			testee.Create(id, new LNL.SourcePathfile(SourcePathfile));
+	
+			var exists = testee.Exists(id);
+			Assert.IsTrue(exists);
+			
+			var res = testee.Get(id);
+			Assert.IsNotNull(res);	//TODO:What is Get really?
+			
+			exists = testee.Exists(id);
+			Assert.IsFalse(exists);
+		}
+
+		private void CreateSourceFile( string pathFile ){
+			System.IO.File.WriteAllText( pathFile, "räksmörgås" );
+		}
+	}
+}
---
+++ LehmaNautaLogic/FileService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LehmaNautaLogic
+{
+	/// <summary>This class is for handling the very physical files.
+	/// As far as files are physical
+	/// </summary>
+	public class FileService
+	{
+		private const string Filename = "X";
+		public string RepositoryPath { get; set; }
+
+		public FileService() { }
+
+		public FileService(string repositoryPath)
+		{
+			this.RepositoryPath = repositoryPath;
+		}
+
+		/// <summary>This method creates a new File in the repository.
+		/// At first glance it might seem to have a bad name or its parameters
+		/// in the wrong order but no.
+		/// It Creates a File with the Unique-Id from the Source-Pathfile.
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="sourcePathfile"></param>
+		public void Create( Guid id, ISourcePathfile sourcePathfile)
+		{
+			var destPath = Path.Combine(RepositoryPath, id.ToString());
+			var destPathfile = Path.Combine( destPath, Filename);
+			Directory.CreateDirectory(destPath);
+			File.Copy(sourcePathfile.Value, destPathfile);
+		}
+
+		/// <summary>This method gets a File.
+		/// It also removes it at once.
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		public string Get(Guid id)
+		{
+			var pathfile = Path.Combine(RepositoryPath, id.ToString(), Filename);
+			var ret = File.ReadAllText( pathfile );
+			File.Delete(pathfile);
+			return ret;
+		}
+
+		//TODO:Will probably be private in the future.
+		public bool Exists(Guid id)
+		{
+			return File.Exists(Path.Combine(RepositoryPath, id.ToString(), Filename));
+		}
+	}
+
+	public interface IPathfile
+	{
+		string Value { get; set; }
+	}
+
+	public interface ISourcePathfile : IPathfile
+	{
+
+	}
+
+	public class SourcePathfile : ISourcePathfile
+	{
+		public string Value { get; set; }
+		public SourcePathfile(){}
+		public SourcePathfile( string pathfile )
+		{
+			Set(pathfile);
+		}
+		private void Set(string pathfile)
+		{
+			this.Value = pathfile;
+		}
+	}
+}
