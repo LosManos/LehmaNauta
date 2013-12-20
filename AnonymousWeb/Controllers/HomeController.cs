0682dcc41cd6cafe99efde61b53fb8288443ec9c
LosManos <ola.fjelddahl@gmail.com>
2013-12-20 22:04:34 +0100

    Uploading to folder works.

---
+++ AnonymousWeb/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnonymousWeb.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			//	Code copied with pride from http://towardsnext.wordpress.com/2009/04/17/file-upload-in-aspnet-mvc/
+			var ret = UploadFiles(Request.Files);
+			return View(ret);
+		}
+
+		private Models.HomeIndexViewmodel UploadFiles(HttpFileCollectionBase requestFiles)
+		{
+			var ret = Models.HomeIndexViewmodel.Create();
+			foreach (string inputTagName in requestFiles)
+			{
+				HttpPostedFileBase file = Request.Files[inputTagName];
+				if (file.ContentLength > 0)
+				{
+					var filePath = Path.Combine(HttpContext.Server.MapPath("~/Uploads"),
+						Path.GetFileName(file.FileName));
+					file.SaveAs(filePath);
+				}
+				ret.Files.Add(
+					new Models.HomeIndexViewmodel.FileInformation(file.FileName, file.ContentLength));
+			}
+			return ret;
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
---
+++ AnonymousWeb/Models/HomeIndexViewmodel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnonymousWeb.Models
+{
+	public class HomeIndexViewmodel
+	{
+		public class FileInformation
+		{
+			public string PathFile { get; set; }
+			public int Length { get; set; }
+			
+			public FileInformation() { }
+			
+			public FileInformation(
+				string pathfile, 
+				int length)
+			{
+				Set(pathfile, length);
+			}
+
+			private void Set(string pathfile, int length)
+			{
+				this.PathFile = pathfile;
+				this.Length = length;
+			}
+		}
+
+		public ICollection<FileInformation> Files;
+
+		public static HomeIndexViewmodel Create()
+		{
+			var ret = new HomeIndexViewmodel
+			{
+				Files = new List<FileInformation>()
+			};
+			return ret;
+		}
+	}	
+}
