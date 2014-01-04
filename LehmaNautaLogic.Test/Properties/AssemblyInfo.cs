f4969b86de87da19b95e0e81471b37df622d5e0a
LosManos <ola.fjelddahl@gmail.com>
2014-01-04 22:18:53 +0100

    Created one version for every project.
    Fixed #14.

--- LehmaNautaLogic.Test/Properties/AssemblyInfo.cs
+++ LehmaNautaLogic.Test/Properties/AssemblyInfo.cs
@@ -8,29 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LehmaNautaLogic.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LehmaNautaLogic.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("25f3529f-8e85-4381-a7c3-005520f5819a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
