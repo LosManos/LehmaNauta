6fe31fee079adf4422c849854c7d3b92e1030aac
LosManos <ola.fjelddahl@gmail.com>
2014-01-18 22:45:30 +0100

    An example of logging is implemented.

---
+++ LehmaNauta.Common/Helpers/Logging.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.Tracing;
+namespace LehmaNauta.Common.Helpers
+{
+	/// <summary>This is the base class for logging. "class"... rather namespace
+	/// masquerading as class.
+	/// We use ETW.
+	/// A somewhat lacking tutorial:
+	/// http://blogs.msdn.com/b/vancem/archive/2012/07/09/logging-your-own-etw-events-in-c-system-diagnostics-tracing-eventsource.aspx
+	/// More info on how to name logs here:
+	/// http://blogs.msdn.com/b/vancem/archive/2012/08/14/etw-in-c-controlling-which-events-get-logged-in-an-system-diagnostics-tracing-eventsource.aspx
+	/// </summary>
+	public static class Logging
+	{
+		/// <summary>This is the base class all ETW event sources should inherit from.
+		/// </summary>
+		public abstract class LehmaNautaEventSource : EventSource
+		{
+			protected const string EventSourceName = "LehmaNauta";
+			protected enum EventId
+			{
+				MyEvent = 1
+			}
+			protected class Keywords
+			{
+				public const EventKeywords Requests = (EventKeywords)0x0001;
+				public const EventKeywords Debug = (EventKeywords)0x0002;
+			}
+		}
+
+		[EventSource(Name = LehmaNautaEventSource.EventSourceName)]
+		public class ExampleEventSource : LehmaNautaEventSource
+		{
+			public static ExampleEventSource Log = new ExampleEventSource();
+
+			[Event( (int)EventId.MyEvent)]//, Keywords=Keywords.Debug)]
+			public void MyEvent(string message)
+			{
+				WriteEvent( (int)EventId.MyEvent, message);
+			}
+		}
+
+	}
+}
---
+++ LehmaNautaLogic.Test/LoggingTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace LehmaNautaLogic.Test
+{
+	[TestClass]
+	public class LoggingTest
+	{
+		/// <summary>This test doesn't really test anything but running a simple log
+		/// so we know it doesn't throw an exception.
+		/// </summary>
+		[TestMethod]
+		public void SimpleLoggingTest()
+		{
+			LehmaNauta.Common.Helpers.Logging.ExampleEventSource.Log.MyEvent("LNeventSrc.MyEventSroucexx");
+		}
+	}
+}
